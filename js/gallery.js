f27f7bf4164d5dc4fb16a28bddd2342756d8e74b
Roman <vrbox@i.ua>
2024-09-23 09:31:27 +0300

    first commit

---
+++ js/gallery.js
@@ -0,0 +1,9 @@
+function makeTransaction(quantity, pricePerDroid) {
+    const mess = `You ordered ${quantity} droids worth ${quantity*pricePerDroid} credits!`;
+    return mess;
+}
+
+console.log("\n**********  Task-1  ************");
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
